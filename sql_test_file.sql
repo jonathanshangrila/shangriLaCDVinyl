b2a394455f318511fdca45cf1299eb8999a85ab6
Jonathan Tuan Tran <116327502+jonathanshangrila@users.noreply.github.com>
2022-12-15 19:08:41 -0800

    example_SQL_file
    For csv values and data to SQL database

---
+++ sql_test_file.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Wood_Variations
+CREATE TABLE Softwoods
+CREATE TABLE Hardwoods
+-- CREATE TABLE other
+
+-- CREATE DATABASE wood_variations_all
+-- CREATE DATABASE softwoods_database
+-- CREATE DATABASE hardwoods_database
+-- CREATE DATABASE other_database
+
+-- SELECT * FROM wood_variations
+-- SELECT * FROM softwoods
+-- SELECT * FROM hardwoods
+-- SELECT * FROM other
+
+INSERT INTO Wood_Varitions (WoodName)
+VALUES (softwoods, hardwoods, sapwood, heartwood, earlywood, springwood, latewood, summerwood, lumber)
+
+INSERT INTO Softwoods (WoodName)
+VALUES (cedar, fir, hemlock, kauri, larch, pine, rimu, redwood, sequioa, spruce, yew)
+
+INSERT INTO Hardwoods (WoodName)
+VALUES (afrormosia, alder, ash, balsa, basswood, beech, birch, blackbean, blackwood, boxwood, brazilwood, bubinga, butternut, cherry, chestnut, cocobolo, ebony, elm, goncalo, hickory, jelutong, kauvala, kingwood, lauan, lime, mahogany, lignum vitae, maple, oak, obeche, padauk, plane, purpleheart, ramin, rosewood, rosewood, satinwood, silky oak, sycamore, teak, tulipwood, utile, walnut, yellow-poplar)
+
+INSERT INTO Other (WoodName)
+VALUES (veneer, plywood, fiberboard)
+
+SELECT WoodName * FROM Softwoods
+
