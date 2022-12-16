43f8f26abb8a75f8ca0f4b22f2390ad11891ad4c
Jonathan Tuan Tran <116327502+jonathanshangrila@users.noreply.github.com>
2022-12-15 18:48:41 -0800

    iOS & Swift App for Shangri-La Music
    First iteration and version on iOS & Swift in XCode

---
+++ ShangriLa_iOS_Main.swift
@@ -0,0 +1,17 @@
+//
+//  shangri_la_appApp.swift
+//  shangri_la_app
+//
+//  Created by Jonathan Tran on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct shangri_la_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
