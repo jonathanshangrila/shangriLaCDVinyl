960886c15644e3408ccdb5b0bc1bfe285aeee60d
Jonathan Tuan Tran <116327502+jonathanshangrila@users.noreply.github.com>
2022-12-20 21:44:21 -0800

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by jonathan on 10/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to Shangri-La Music!")
+        Text("")
+        Text("The CD & Vinyl")
+        Text("stream-and-mail-to-home,")
+        Text("subscription app!")
+        Text("Visit the website currently under construction.")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExploringXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploringXcodeApp.swift
+//  Shared
+//
+//  Created by jonathan on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ExploringXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
